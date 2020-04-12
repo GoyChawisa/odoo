e6b063c1717095b27c574ba486167cfe857a2e17
GoyChawisa <59275070+GoyChawisa@users.noreply.github.com>
2020-04-12 20:15:54 +0700

    Add files via upload

---
+++ addons/hospital/models/patient.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models, api
+
+
+class Patient(models.Model):
+    _name = 'hospital.patient'
+    _description = "Hospital Patient"
+
+    name = fields.Char(string="Name", required=True)
+    age = fields.Integer(string="Age", required=True)
+    age_group = fields.Char(string="Age Group", compute='set_age_group', store=True)
+    gender = fields.Selection(selection=[('Male','Male'),('Female','Female'),],string='Gender', default='Male', required=True)
+    note = fields.Text()
+
+    @api.depends('age')
+    def set_age_group(self):
+        for r in self:
+            if r.age > 20:
+                r.age_group = 'major'
+            else:
+                r.age_group = 'minor'
