b85c966921fd5df4de502a0192a9833d8884e06d
GoyChawisa <chawisa.ksr@gmail.com>
2020-04-20 16:15:30 +0700

    edit bug

--- addons/hospital/models/appointment.py
+++ addons/hospital/models/appointment.py
@@ -6,11 +6,11 @@ class Doctor(models.Model):
     _name = 'hospital.appointment'
     _description = "Hospital Appointment"
 
-    patient = fields.Many2one('hospital.patient', ondelete='set null', string="Patient name", index=True)
+    patient = fields.Many2one('hospital.patient', required=True, ondelete='set null', string="Patient name", index=True)
     patient_code_name = fields.Char(string="Patient", compute='get_patient')
-    doctor = fields.Many2one('hospital.doctor', ondelete='set null', string="Docter name", index=True)
+    doctor = fields.Many2one('hospital.doctor', required=True, ondelete='set null', string="Docter name", index=True)
     doctor_code_name = fields.Char(string="Doctor", compute='get_doctor')
-    dateTime = fields.Datetime('Date time', required=False, readonly=False,select=True, index=True, default=lambda self:fields.datetime.now())
+    dateTime = fields.Datetime('Date time', required=True, readonly=False, index=True, default=lambda self:fields.datetime.now())
 
     @api.depends('patient')
     def get_patient(self):
